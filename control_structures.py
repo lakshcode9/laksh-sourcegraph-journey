79a4b360ff5e7220103209a91193133021a6db1d
Laksh <support@lakshpujary.me>
2025-07-19 13:07:30 +0530

    ...

---
+++ control_structures.py
@@ -0,0 +1,7 @@
+attempts = 2
+
+if attempts > 3:
+    print("Account locked")
+else:
+    print("Try again. You still have access.")
+    
---
+++ variables_and_types.py
@@ -0,0 +1,7 @@
+username = "Laksh"
+attempts = 0
+open_ports = [22, 80, 443]
+
+print("Username:", username)
+print("Login attempts:", attempts)
+print("Open ports",open_ports)
